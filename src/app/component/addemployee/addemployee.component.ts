daefe158cbf337ae88e26db511895b47b6c52f84
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-03-17 15:02:40 +0300

    commit

--- src/app/component/addemployee/addemployee.component.ts
+++ src/app/component/addemployee/addemployee.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 @Component({
   selector: 'app-addemployee',
-  imports: [],
+  imports: [MatCardModule, ReactiveFormsModule,MatFormFieldModule,MatButtonModule,MatInputModule,MatSelectModule,MatDatepickerModule],
   templateUrl: './addemployee.component.html',
   styleUrl: './addemployee.component.css'
 })
 export class AddemployeeComponent {
 
+  header = 'Add New'
+
+  empForm = new FormGroup({
+    id: new FormControl(0),
+    name: new FormControl("", Validators.required),
+    doj: new FormControl(new Date, Validators.required),
+    role: new FormControl("", Validators.required),
+    salary: new FormControl(0, Validators.required)
+  })
+
+
+  SaveEmp(){
+    if(this.empForm.valid){
+
+    }
+  }
 }
--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { AddemployeeComponent } from '../addemployee/addemployee.component';
 
 @Component({
   selector: 'app-employee',
-  imports: [MatCardModule,MatButtonModule,MatIconModule],
+  imports: [MatCardModule,MatButtonModule,MatIconModule,MatDialogModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
+constructor(private dialog:MatDialog){}
 
+  addEmployee(){
+    this.dialog.open(AddemployeeComponent)
+  }
 }
