8fa7c3a91c9bce46c63a76428cdb41c7e5033e57
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-03-19 14:34:24 +0300

    commit

--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { AddemployeeComponent } from '../addemployee/addemployee.component';
+import { Employee } from '../../models/employeeModelDTO';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { EmployeeService } from '../../services/employee.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee',
-  imports: [MatCardModule,MatButtonModule,MatIconModule,MatDialogModule],
+  imports: [MatCardModule,MatButtonModule,MatIconModule,MatDialogModule,MatTableModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
-constructor(private dialog:MatDialog){}
+export class EmployeeComponent implements OnInit, OnDestroy{
+
+emplist:Employee[]=[];
+datatable!:MatTableDataSource<Employee>;
+displayedColmuns:string[]=['id','name','role','doj','salary','action'];
+subscription=new Subscription()
+dataSource!: MatTableDataSource<any>;
+
+
+constructor(private dialog:MatDialog, private service:EmployeeService){}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+ 
+
+ngOnInit(): void {
+   
+  }
 
   addEmployee(){
     this.dialog.open(AddemployeeComponent)
+  }
+
+  getAllEmp(){
+    let sub=this.service.getAll().subscribe(resp=>{
+      this.emplist=resp;
+      this.dataSource=new MatTableDataSource(this.emplist)
+    })
+    this.subscription.add(sub);
   }
 }
